0a3bafe645efac30b8aaa4bbb4fb7b60e3417ab0
batlimos <abderrahman.batlamouss@gmail.com>
2020-02-03 17:57:44 +0100

    firt componenent

---
+++ src/components/WelcomeMassege.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="welcome-massege">
+        <h2>welcome massege</h2>
+        <p class="mytxt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sit, dolore quos voluptate doloremque fugit quas a at autem, et voluptatem perspiciatis porro. A laborum quidem veritatis itaque necessitatibus animi!</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "WelcomeMassege"
+}
+</script>
+
+<style scoped>
+h2
+{
+    color: rgb(11, 167, 115);
+}
+.mytxt
+{
+    color: #777;
+    font-weight: 600;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <WelcomeMassege/>
   </div>
 </template>
+
+<script>
+import WelcomeMassege from "@/components/WelcomeMassege.vue";
+
+export default {
+  components: {
+    WelcomeMassege,
+  }
+};
+</script>
